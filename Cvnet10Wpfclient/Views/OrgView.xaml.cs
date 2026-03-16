9748b15d09bdf8a5b11ba70dee676e9fe85a0755
Sekiya Sato <sekiya.sato@gmail.com>
2026-03-16 17:56:07 +0900

    プロジェクトファイルの整理

---
+++ Cvnet10Wpfclient/Views/OrgView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cvnet10Wpfclient.Views;
+
+/// <summary>
+/// ViewOrg.xaml の相互作用ロジック
+/// </summary>
+public partial class OrgView : Helpers.BaseWindow {
+	public OrgView() {
+		InitializeComponent();
+	}
+}
